04917a7b413cc7e4a00598342c6b03821f066193
asghar9613-png <asghar.9613@gmail.com>
2026-06-27 02:56:45 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("سلام اصغر، اولین پروژه GitHub با موفقیت ساخته شد!")
+import os
+import requests
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+message = "🎉 سلام اصغر!\n\nاولین پیام از GitHub با موفقیت ارسال شد."
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": message
+})
+
+print("Message sent successfully!")
